25cd82f371e76495bebf4a24c5b512322384c797
Ryangraves93 <ryangraves1993@gmail.com>
2020-04-04 17:55:10 +0100

    First commit

---
+++ Assets/Assets/Custom RP/Runtime/CameraRenderer.Editor.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEditor;
+using UnityEngine.Profiling;
+partial class CameraRenderer 
+{
+    partial void DrawGizmos();
+    partial void DrawUnsupportedShaders();
+    partial void PrepareForSceneWindow();
+
+    partial void PrepareBuffer();
+#if UNITY_EDITOR
+    static ShaderTagId[] legacyShaderTagIds =
+    {
+        new ShaderTagId("Always"),
+        new ShaderTagId("ForwardBase"),
+        new ShaderTagId("PrepassBase"),
+        new ShaderTagId("Vertex"),
+        new ShaderTagId("VertexLMRGBM"),
+        new ShaderTagId("VertexLM")
+    };
+
+    string SampleName { get; set; }
+    static Material errorMaterial;
+
+    partial void DrawGizmos()
+    {
+        if (Handles.ShouldRenderGizmos())
+        {
+            context.DrawGizmos(camera, GizmoSubset.PreImageEffects);
+            context.DrawGizmos(camera, GizmoSubset.PostImageEffects);
+        }
+    }
+
+    partial void PrepareForSceneWindow()
+    {
+        if (camera.cameraType == CameraType.SceneView)
+        {
+            ScriptableRenderContext.EmitWorldGeometryForSceneView(camera);
+        }
+    }
+    partial void DrawUnsupportedShaders()
+    {
+
+        if (errorMaterial == null)
+        {
+            errorMaterial = new Material(Shader.Find("Hidden/InternalErrorShader"));
+        }
+        var drawSettings = new DrawingSettings(legacyShaderTagIds[0], new SortingSettings(camera)
+            )
+        {
+            overrideMaterial = errorMaterial
+        };
+
+        for (int i = 1; i < legacyShaderTagIds.Length; i++)
+        {
+            drawSettings.SetShaderPassName(i, legacyShaderTagIds[i]);
+        }
+        var filteringSettings = FilteringSettings.defaultValue;
+        context.DrawRenderers(cullingResults, ref drawSettings, ref filteringSettings);
+    }
+
+    partial void PrepareBuffer()
+    {
+        Profiler.BeginSample("Editor Only");
+        buffer.name = SampleName = camera.name;
+        Profiler.EndSample();
+    }
+#else
+    const string SampleName = bufferName;
+#endif
+}
---
+++ Assets/Assets/Custom RP/Runtime/CustomRenderPipelineAsset.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+[CreateAssetMenu(menuName = "Render/Custom Render Pipeline")]
+public class CustomRenderPipelineAsset : RenderPipelineAsset 
+
+{
+    
+    protected override RenderPipeline CreatePipeline()
+    {
+        return new CustomRenderPipeline();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
